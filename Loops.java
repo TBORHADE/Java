9bfd3b3ca1582fca3376047069e61a2436b8b5fd
Tanavi <tanaviprakashborhade.com>
2023-08-19 23:20:46 +0530

    loops file added

---
+++ Loops.java
@@ -0,0 +1,22 @@
+public class Loops {
+    public static void main(String args[]){
+
+        // for(int i = 0; i<11; i++){
+        //     System.out.println(i);// forloop
+        // }
+        //System.out.println("Hello World ")
+
+        // int i = 0;
+        // while(i < 11){
+        //     System.out.println(i);
+        //     i = i + 1; // i++
+        // }
+
+        int i = 0;
+        do{
+            System.out.println(i);
+            i = i + 1;
+        } while(i<11);
+
+    }
+}
