69bc3085cc8641e95e377bc63be3706bb234b8d6
Tanavi <tanaviprakashborhade.com>
2023-08-22 00:44:22 +0530

    service file added

---
+++ ServiceProvider.java
@@ -0,0 +1,16 @@
+interface Interf
+{
+    public void m1();
+    public void m2();
+}
+class ServiceProvider implements Interf
+{
+    public void m1()
+    {
+        
+    }
+    public void m2()
+    {
+        
+    }
+}
